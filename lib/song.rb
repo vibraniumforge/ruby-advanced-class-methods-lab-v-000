d4daf15e6c5ed805cb6252c5fbb1aa6e24b00000
Greg Morgan <gregm9999@gmail.com>
2018-11-10 00:36:43 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,9 +22,9 @@ class Song
     song
   end
 
-  def self.create_by_name(name)
+  def self.create_by_name(song_name)
     song=self.new
-    song.name=name
+    song.name=song_name
     song.save
     song
   end
